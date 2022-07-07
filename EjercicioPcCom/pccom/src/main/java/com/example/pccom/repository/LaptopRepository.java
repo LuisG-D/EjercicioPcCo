c81395f76216f69d897e13ee605d707cfc59b24a
LuisG-D <69773128+LuisG-D@users.noreply.github.com>
2022-07-07 17:16:18 +0200

    Delete EjercicioPcCom/pccom directory

--- EjercicioPcCom/pccom/src/main/java/com/example/pccom/repository/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.pccom.repository;
-
-import com.example.pccom.entities.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LaptopRepository extends JpaRepository<Laptop,Long> {
-}
